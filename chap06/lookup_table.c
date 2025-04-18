db977d082acc4c7f2cdc8298851cbfd1e67dfc62
ybertoldi <yuri.bertoldi003@gmail.com>
2025-04-18 14:19:51 -0300

    testando

--- chap06/lookup_table.c
+++ chap06/lookup_table.c
@@ -10,7 +10,6 @@ struct nlist{
 };
 static struct nlist *hashtab[HASHSIZE];
 
-unsigned hash(char *s){
     unsigned hashval;
     for (hashval = 0; *s != '\0'; s++){
         hashval = *s + 31 * hashval;
@@ -18,7 +17,6 @@ unsigned hash(char *s){
     return hashval % HASHSIZE;
 }
 struct nlist *lookup(char *s){
-    struct nlist *np;
 
     for (np = hashtab[hash(s)]; np != NULL; np = np->next){
         if (strcmp(s, np->name) == 0){
@@ -27,9 +25,9 @@ struct nlist *lookup(char *s){
     }
 
     return NULL;
+
 }
 struct nlist* install(char* name, char* defn)
-{
     struct nlist* np;
     unsigned hashval;
 
